191645b773c33a867eb728d2b95ad43477c4b5b6
Martin Valchev <mv1u18@soton.ac.uk>
2019-03-18 00:32:20 +0000

    recipe editing basic layout

--- src/main/java/comp1206/sushi/server/controllers/DishesController.java
+++ src/main/java/comp1206/sushi/server/controllers/DishesController.java
@@ -1,5 +1,6 @@
 package comp1206.sushi.server.controllers;
 
+import com.jfoenix.controls.JFXButton;
 import comp1206.sushi.common.Dish;
 import comp1206.sushi.server.components.NumericTableCell;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -11,29 +12,31 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.PopOver;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DishesController extends MainViewController implements Initializable {
 
-    @FXML
-    private TableView<Dish> dishesTable;
-    @FXML
-    private TableColumn<Dish, String> name;
-    @FXML
-    private TableColumn<Dish, String> description;
-    @FXML
-    private TableColumn<Dish, Number> price;
-    @FXML
-    private TableColumn<Dish, Number> restockThreshold;
-    @FXML
-    private TableColumn<Dish, Number> restockAmount;
-    @FXML
-    private TableColumn<Dish, Number> stock;
+    private PopOver popOver;
+    @FXML private AnchorPane recipe;
+    @FXML private TableView<Dish> dishesTable;
+    @FXML private TableColumn<Dish, String> name;
+    @FXML private TableColumn<Dish, String> description;
+    @FXML private TableColumn<Dish, Number> price;
+    @FXML private TableColumn<Dish, Number> restockThreshold;
+    @FXML private TableColumn<Dish, Number> restockAmount;
+    @FXML private TableColumn<Dish, Number> stock;
+    @FXML private JFXButton editRecipeButton;
+    protected Dish currentlySelectedDish;
 
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
+
+        popOver = new PopOver(recipe);
+        popOver.setHeaderAlwaysVisible(true);
 
         //---------------------Name Column---------------------------------
         name.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -52,12 +55,12 @@ public class DishesController extends MainViewController implements Initializabl
 
         //---------------------Restock Threshold Column---------------------------------
         restockThreshold.setCellValueFactory(new PropertyValueFactory<>("restockThreshold"));
-        restockThreshold.setCellFactory(column -> new NumericTableCell<>(0,0));
+        restockThreshold.setCellFactory(column -> new NumericTableCell<>(0, 0));
         restockThreshold.setOnEditCommit(e -> dishesTable.getSelectionModel().getSelectedItem().setRestockThreshold(e.getNewValue()));
 
         //---------------------Restock Amount Column---------------------------------
         restockAmount.setCellValueFactory(new PropertyValueFactory<>("restockAmount"));
-        restockAmount.setCellFactory(c -> new NumericTableCell<>(0,0));
+        restockAmount.setCellFactory(c -> new NumericTableCell<>(0, 0));
         restockAmount.setOnEditCommit(e -> dishesTable.getSelectionModel().getSelectedItem().setRestockAmount(e.getNewValue()));
 
         //---------------------Stock Column---------------------------------
@@ -66,5 +69,13 @@ public class DishesController extends MainViewController implements Initializabl
         ObservableList<Dish> dishData = FXCollections.observableList(server.getDishes());
         dishesTable.setItems(dishData);
 
+        dishesTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) currentlySelectedDish = dishesTable.getSelectionModel().getSelectedItem();
+            popOver.setTitle(currentlySelectedDish.getName() + "'s recipe");
+        });
+
+        editRecipeButton.setOnAction(e -> {
+            if (currentlySelectedDish != null) popOver.show(editRecipeButton);
+        });
     }
 }
---
+++ src/main/java/comp1206/sushi/server/controllers/RecipeController.java
@@ -0,0 +1,15 @@
+package comp1206.sushi.server.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RecipeController extends DishesController implements Initializable {
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("hh");
+    }
+}
